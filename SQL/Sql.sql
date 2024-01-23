b24a03a64701847b28ff76e1b459284e68e88a96
Felix9908 <frfyfelix@gmail.com>
2024-01-23 15:51:44 -0500

    insert file  SQL

---
+++ SQL/Sql.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    token VARCHAR(25),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE post (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    post TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+insert into post (title,post,user_id)
+ values
+('New post post','post Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet',1),
+('New post','post Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet',1);
+
+insert into users (name,post)
+ values
+('UserName','Password');
+
