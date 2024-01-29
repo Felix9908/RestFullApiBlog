04ed0b6c05169a9f0f7944d0cf848794d28e16d0
Felix9908 <frfyfelix@gmail.com>
2024-01-29 10:49:08 -0500

    changes in sql file

--- SQL/Sql.sql
+++ SQL/Sql.sql
@@ -1,28 +1,9 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    token VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
-CREATE TABLE post (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    post TEXT NOT NULL,
-    user_id INTEGER REFERENCES users(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 insert into post (title,post,user_id)
  values
 ('New post post','post Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet',1),
 ('New post','post Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet',1);
 
-insert into users (name,post)
+insert into users (name,password)
  values
-('UserName','Password');
+('UserName','1234');
 
